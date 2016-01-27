c5bbc299d5f358642b4642b657815a2ad8b98dc1
Matthew Griffith <mdg.griffith@gmail.com>
2016-01-27 18:45:06 -0500

    Sketch of project

---
+++ Color/Mixing.elm
@@ -0,0 +1,44 @@
+module Color.Mixing where
+
+{-|
+
+
+-}
+
+import Color exposing (..)
+
+type alias Percentage = Float
+
+
+
+-- Functions that will be covered:
+
+
+--saturate : Color -> Percentage
+--saturate c = 0.0
+
+--desaturate : Color -> Percentage
+--desaturate c = 0.0
+
+--lighten
+--darken
+--fadein
+--fadeout
+--fade
+--spin
+--mix
+--tint
+--shade
+--greyscale
+--contrast
+
+
+--multiply
+--screen
+--overlay
+--softlight
+--hardlight
+--difference
+--exclusion
+--average
+--negation
---
+++ README.md
@@ -0,0 +1,12 @@
+# Elm Color Mixing
+
+At the moment this package is just a stub.  
+
+My plan is to cover all color functions that are documented in the LessCss CSS preprocessor:
+
+[http://lesscss.org/functions/#color-operations](http://lesscss.org/functions/#color-operations)
+
+And to use the [Core Color type](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Color) provided by Elm.
+
+
+
