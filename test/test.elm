01a9777d9388c272ef8ec6d69539e6a42cc2a9af
Matthew Griffith <mdg.griffith@gmail.com>
2016-01-29 15:32:25 -0500

    Add testing suite

---
+++ test/test.elm
@@ -0,0 +1,73 @@
+module Main (..) where
+
+import String
+import Graphics.Element exposing (Element)
+import ElmTest exposing (..)
+import Debug
+import Color.Mixing exposing (..)
+import Color exposing (..)
+
+
+startColor =
+    hsl (degrees 90) 0.8 0.5
+
+
+colorEquality c1 c2 =
+    let
+        rgba1 = Debug.log "color1" <| toRgb c1
+
+        rgba2 = Debug.log "color2" <| toRgb c2
+
+        alphaTolerance a =
+            round (a * 1000)
+    in
+        rgba1.red
+            == rgba2.red
+            && rgba1.green
+            == rgba2.green
+            && rgba1.blue
+            == rgba2.blue
+            && alphaTolerance rgba1.alpha
+            == alphaTolerance rgba2.alpha
+
+
+tests : Test
+tests =
+    suite
+        "Elm Color Mixing Testing Suite!"
+        [ test "Saturate"
+            <| assert
+            <| colorEquality (saturate 0.2 startColor) (hsl (degrees 90) 1 0.5)
+        , test "Desaturate"
+            <| assert
+            <| colorEquality (desaturate 0.2 startColor) (hsl (degrees 90) 0.6 0.5)
+        , test "Lighten"
+            <| assert
+            <| colorEquality (lighten 0.2 startColor) (hsl (degrees 90) 0.8 0.7)
+        , test "Darken"
+            <| assert
+            <| colorEquality (darken 0.2 startColor) (hsl (degrees 90) 0.8 0.3)
+        , test "FadeIn"
+            <| assert
+            <| colorEquality (fadeIn 0.1 (hsla (degrees 90) 0.8 0.5 0.8)) (hsla (degrees 90) 0.8 0.5 0.9)
+        , test "FadeOut"
+            <| assert
+            <| colorEquality (fadeOut 0.1 (hsla (degrees 90) 0.8 0.5 0.8)) (hsla (degrees 90) 0.8 0.5 0.7)
+        , test "Fade"
+            <| assert
+            <| colorEquality (fade 0.1 (hsla (degrees 90) 0.8 0.5 0.8)) (hsla (degrees 90) 0.8 0.5 0.1)
+        , test "Spin"
+            <| assert
+            <| colorEquality (spin (degrees 30) (hsl (degrees 10) 0.9 0.5)) (hsl (degrees 40) 0.9 0.5)
+        , test "Mix"
+            <| assert
+            <| colorEquality (mix 0.5 (rgba 100 0 0 1.0) (rgba 0 100 0 0.5)) (rgba 75 25 0 0.75)
+        , test "Greyscale"
+            <| assert
+            <| colorEquality (toGreyscale <| hsl (degrees 90) 0.9 0.5) (hsl (degrees 90) 0 0.5)
+        ]
+
+
+main : Element
+main =
+    elementRunner tests
